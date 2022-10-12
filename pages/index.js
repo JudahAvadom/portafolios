b3d5d37674991573d493474c08b98e4eb2b8ac98
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2022-10-11 20:06:59 -0500

    Init

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Gerset Linarez | Full stack Developer</title>
+        <meta name="description" content="I’m a fullstack developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+      <Main />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  )
+}
