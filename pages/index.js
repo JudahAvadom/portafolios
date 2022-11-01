4c517dce7c538fae2cd70d586245fb146e943fce
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2022-11-01 16:18:22 -0500

    Meta tags

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ export default function Home() {
         <title>Gerset Linarez | Full stack Developer</title>
         <meta name="description" content="I’m a fullstack developer specializing in building (and occasionally designing) exceptional digital experiences." />
         <link rel="icon" href="/fav.png" />
+        <meta property="og:title" content="Gerset Linarez" />
+        <meta property="og:type" content="Portfolio" />
+        <meta property="og:url" content="https://gerset.netlify.app/" />
+        <meta property="og:image" content="/fav.png" />
+        <meta property="og:description" content="I’m a fullstack developer specializing in building (and occasionally designing) exceptional digital experiences." />
       </Head>
       <Main />
       <About />
