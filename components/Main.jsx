5511614e05ff7a910e5bffe93f9fd7155243ddb9
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2022-11-01 21:14:52 -0500

    DEveloper

--- components/Main.jsx
+++ components/Main.jsx
@@ -40,7 +40,7 @@ const Main = () => {
           <h1 className='py-4 text-gray-700'>
             Hi, I&#39;m <span className='text-[#5651e5]'> Gerset Linarez</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A Full Stack Web Developer</h1>
+          <h1 className='py-2 text-gray-700'>A Full Stack Developer</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
             I’m focused on building fullstack web and mobile applications
             with the best technologies.
